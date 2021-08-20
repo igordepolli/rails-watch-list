f917789a434f343e4f0d6c10891b8e1695ecc472
Igor Depolli <igorcler@gmail.com>
2021-08-20 11:27:58 -0300

    Add dotenv - Protect my secret data in .env file

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -19,6 +19,7 @@ class BookmarksController < ApplicationController
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
+    redirect_to request.referrer
   end
 
   private
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,5 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :comment, length: { minimum: 6 }
-  validates :movie_id, uniqueness: { scope: :list }
+  validates :movie, uniqueness: { scope: :list, message: 'already bookmarked' }
 end
